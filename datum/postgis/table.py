5c9a4a8e4807e169cda5ef1973006975e687f6b7
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2016-09-12 08:20:19 -0400

    Map the REAL data type to NUM

--- datum/postgis/table.py
+++ datum/postgis/table.py
@@ -8,6 +8,7 @@ FIELD_TYPE_MAP = {
     'integer':              'num',
     'numeric':              'num',
     'double precision':     'num',
+    'real':                 'num',
     'text':                 'text',
     'character varying':    'text',
     'date':                 'date',
