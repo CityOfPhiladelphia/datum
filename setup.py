ab6fb254dc58e5aa690ac533fcf34834249a2306
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2016-09-12 08:19:39 -0400

    Specify shapely as a requirement for Oracle adapter

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(name='datum',
       packages=['datum'],
       install_requires=['six==1.10.0'],
       extras_require={
-        'oracle_stgeom': ['cx-Oracle==5.2.1', 'pyproj==1.9.5.1'],
+        'oracle_stgeom': ['cx-Oracle==5.2.1', 'pyproj==1.9.5.1', 'shapely==1.5.17'],
         'postgis': ['psycopg2==2.6.1'],
       },
       # entry_points={'console_scripts': ['ais=ais:manager.run']},
